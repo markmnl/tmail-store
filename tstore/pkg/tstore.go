aef21135ba9ec133f8e2cba4a2158f343e5fefdc
markmnl <markmnl@outlook.com>
2020-01-22 23:10:39 +0800

    types

--- tstore/pkg/tstore.go
+++ tstore/pkg/tstore.go
@@ -2,24 +2,27 @@ package tstore
 
 // Msg is a message
 type Msg struct {
-	From string
-	To string
-	Time int64
-	Content string
+	ID		string	`json:"id"`
+	PID		string	`json:"pid"`
+	From 	string	`json:"from"`
+	To 		string	`json:"to"`
+	Time 	int64	`json:"time"`
+	Topic	string	`json:"topic"`
+	Content string	`json:"content"`
 }
 
 // MsgStorer defines the Store(msg) method to store a Msg
 type MsgStorer interface {
-	Store(msg Msg) error
+	Store(msg *Msg) error
 }
 
 // MsgFetcher defines methods for retreiving messages from a store
 type MsgFetcher interface {
-	Fetch(id string) error
+	Fetch(id *string) error
 	FetchLatest(topic string, maxDepth int) error
 }
 
 // MsgSearcher defines the Search(str) method to search a message store 
 type MsgSearcher interface {
-	Search(str string) []Msg
+	Search(str *string) []Msg
 }
