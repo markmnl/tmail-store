d00ca841ef32ab5b374886870b9a328f3e819e85
markmnl <markmnl@outlook.com>
2020-01-25 14:33:44 +0800

    added type to Msg

--- tstore/pkg/tstore.go
+++ tstore/pkg/tstore.go
@@ -8,6 +8,7 @@ type Msg struct {
 	To 		string	`json:"to"`
 	Time 	int64	`json:"time"`
 	Topic	string	`json:"topic"`
+	Type 	string	`json:"type"`
 	Content string	`json:"content"`
 }
 
@@ -18,11 +19,11 @@ type MsgStorer interface {
 
 // MsgFetcher defines methods for retreiving messages from a store
 type MsgFetcher interface {
-	Fetch(id *string) error
-	FetchLatest(topic string, maxDepth int) error
+	Fetch(id *string) (Msg, error)
+	FetchLatest(topic string, maxDepth int) ([]Msg, error)
 }
 
 // MsgSearcher defines the Search(str) method to search a message store 
 type MsgSearcher interface {
-	Search(str *string) []Msg
+	Search(str *string) ([]Msg, error)
 }
