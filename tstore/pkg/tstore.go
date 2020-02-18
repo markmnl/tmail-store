689f699f1f948f26c119a270a7a610f3379b79aa
markmnl <markmnl@outlook.com>
2020-02-18 21:56:24 +0800

    multiple recepients

--- tstore/pkg/tstore.go
+++ tstore/pkg/tstore.go
@@ -6,24 +6,24 @@ type Msg struct {
 	PID		[32]byte	
 	PID64	string		`json:"pid64"`
 	From 	string		`json:"from"`
-	To 		string		`json:"to"`
+	To 		[]string	`json:"to"`
 	Time 	int64		`json:"time"`
 	Topic	string		`json:"topic"`
 	Type 	string		`json:"type"`
 	Content string		`json:"content"`
 }
 
-// MsgVerifyer defines the ParentExists(msg) method 
+// MsgVerifyer defines the ParentExists(msg) function 
 type MsgVerifyer interface {
-	ParentExists(msg *Msg) (bool, error)
+	ParentExists(pSHA256 []byte) (bool, error)
 }
 
-// MsgStorer defines the Store(msg) method to store a Msg
+// MsgStorer defines the Store(msg) function to store a Msg
 type MsgStorer interface {
 	Store(msg *Msg) error
 }
 
-// MsgFetcher defines methods for retreiving messages from a store
+// MsgFetcher defines function for retreiving messages from a store
 type MsgFetcher interface {
 	Fetch(id *string) (Msg, error)
 	FetchLatest(topic string, maxDepth int) ([]Msg, error)
